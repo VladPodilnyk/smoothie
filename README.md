3c9e6a82d0ce1d87145ef762b47b8bf44142b5e0
VladPodilnyk <vladyslav.podilnyk@bolt.eu>
2024-01-31 22:28:49 +0200

    make it simple.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # smoothie
 
-A rate limiter that smoothe your day.
-The repository contains an old draft implementation that is written in Scala.
-Since maintaining Scala is a huge burden, the project will be rewriten in Go.
+A dead simple distributed rate limiter that smoothe your day.
+
+🚧 Readme section is under construction 🚧 
--- ratelimiter/ratelimiter.go
+++ ratelimiter/ratelimiter.go
@@ -3,37 +3,67 @@ package ratelimiter
 import (
 	"context"
 	"errors"
+	"strconv"
+	"time"
 
-	"github.com/vladpodilnyk/smoothie/internal/storage"
+	"github.com/redis/go-redis/v9"
 )
 
-// holds strategy and configuration values
-type RateLimiter struct {
-	storage storage.Storage
-	rate    Rate
+var redisScript = redis.NewScript(`
+	if tonumber(redis.call('incr', KEYS[1])) == 1 then
+		return redis.call('expireAt', KEYS[1], ARGV[1])
+	else
+		return 0
+	end
+`)
+
+type Rate struct {
+	NumberOfRequests uint
+	Duration         time.Duration
 }
 
-func New() *RateLimiter {
+type RateLimiter struct {
+	client *redis.Client
+	rate   Rate
+}
+
+func New(config *redis.Options, rate Rate) *RateLimiter {
 	return &RateLimiter{}
 }
 
-func (limiter *RateLimiter) Exec(ctx context.Context, key string, effect func()) error {
+func (limiter *RateLimiter) Exec(ctx context.Context, key string, effect func() error) error {
 	if limiter.Allow(ctx, key) {
-		effect()
-		return nil
+		maybeError := effect()
+		return maybeError
 	}
 	return errors.New(("Limit exceeded, please try again later."))
 }
 
 func (limiter *RateLimiter) Allow(ctx context.Context, key string) bool {
-	counter, err := limiter.storage.Get(ctx, key)
+	counter, err := limiter.get(ctx, key)
 	if err != nil {
 		return false
 	}
 
-	limiter.storage.Inc(ctx, key)
+	limiter.inc(ctx, key)
 	if counter > limiter.rate.NumberOfRequests+1 {
 		return false
 	}
 	return true
+}
+
+func (limiter *RateLimiter) inc(ctx context.Context, key string) {
+	redisScript.Run(ctx, limiter.client, []string{key}, time.Now().Add(limiter.rate.Duration).Unix())
+}
+
+func (limiter *RateLimiter) get(ctx context.Context, key string) (uint, error) {
+	result := limiter.client.Get(ctx, key)
+	if result.Err() != nil {
+		return 0, result.Err()
+	}
+	maybeInt, err := strconv.Atoi(result.Val())
+	if err != nil {
+		return 0, err
+	}
+	return uint(maybeInt), nil
 }
