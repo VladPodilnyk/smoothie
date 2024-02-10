ee2660a72e5ad74f3cf632571d1ef752662f82ed
VladPodilnyk <vladyslav.podilnyk@bolt.eu>
2024-02-10 23:56:54 +0200

    update README and add comments.

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
-# smoothie
+# Smoothie
 
-A dead simple distributed rate limiter that smoothe your day.
+A dead simple distributed rate limiter that smoothes your day.
 
-🚧 Readme section is under construction 🚧 
+### Yet another rate limiter?
+Smoothie is meant to be a distributed rate limiter, unlike all other popular within go community
+solutions. The project is quite small so, it doesn't make
+any sense to publish it as a package. But it can be a good showcase of how to
+make a distributed rate limiter using redis and Go.
+If you don't need that or maybe you don't like Redis, 
+then standard `time` package or `rate` package might serve you better.
+
+### Implementation details
+At the moment smoothie implements a fixed-size window strategy for limiting requests.
+The api resembles the familiar `rate` package API with `Allow` and `Exec` methods.
+See example below
+```go
+config := redis.Options{
+	Addr:     "localhost:6379",
+	Password: "",
+	DB:       0,
+}
+limiter := New(&testRedisOptions, Rate{NumberOfRequests: 1, Duration: 5 * time.Second})
+isAllowed := limiter.Allow(ctx.Background(), "user-IP-address")
+testEffect := func () error {
+    fmt.Println("hello")
+    return nil
+}
+err := limiter.Exec(ctx.Background(), "user-IP-adress", testEffect)
+```
+
+### Future plans
+It would be nice to add the following things to the smoothie:
+- other rate limiting strategies (Sliding window, token bucket)
+- extend tests with test cases with concurrent access to the same limiting `key`
--- ratelimiter/ratelimiter.go
+++ ratelimiter/ratelimiter.go
@@ -3,7 +3,6 @@ package ratelimiter
 import (
 	"context"
 	"errors"
-	"fmt"
 	"time"
 
 	"github.com/redis/go-redis/v9"
@@ -19,16 +18,19 @@ end
 return current
 `)
 
+// Rate represents the rate limter configuration
 type Rate struct {
-	NumberOfRequests uint
-	Duration         time.Duration
+	NumberOfRequests uint          // Number of requests allowed
+	Duration         time.Duration // Time window
 }
 
+// RateLimiter holds the redis client and the rate configuration
 type RateLimiter struct {
 	client *redis.Client
 	rate   Rate
 }
 
+// Creates a new rate limiter with the given redis config and rate
 func New(config *redis.Options, rate Rate) *RateLimiter {
 	// Typically it's better to pass client as a parameter, but
 	// since Smoothie stricly depends on redis, it doesn't make
@@ -37,15 +39,19 @@ func New(config *redis.Options, rate Rate) *RateLimiter {
 	return &RateLimiter{client, rate}
 }
 
+// Exec executes the given function (effect) if the rate limit is not exceeded,
+// otherwise it returns an error. A user should pass a key that uniquely identifies
+// the request.
 func (limiter *RateLimiter) Exec(ctx context.Context, key string, effect func() error) error {
 	if limiter.Allow(ctx, key) {
-		fmt.Println("Allowing request")
 		maybeError := effect()
 		return maybeError
 	}
 	return errors.New(("Limit exceeded, please try again later."))
 }
 
+// Allow returns true if the rate limit is not exceeded, otherwise it returns false.
+// A user should pass a key that uniquely identifies the request.
 func (limiter *RateLimiter) Allow(ctx context.Context, key string) bool {
 	currentValue := limiter.incrementAndGet(ctx, key)
 	if currentValue > limiter.rate.NumberOfRequests {
